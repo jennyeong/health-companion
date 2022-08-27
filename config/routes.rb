c326dcba69a0bd0ac913dd3fa43d8aa681f33939
Chee Jenn Yeong <cheejennyeong95@gmail.com>
2022-08-27 16:16:32 +0800

    adding medicines model & controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'medicines/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :medicines, only: [:show]
 
   # Defines the root path route ("/")
   # root "articles#index"
