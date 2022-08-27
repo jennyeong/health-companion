053d9a3423ca3a9b20a2eecd0c771445707054ba
Ye Zhu <dellazhuye@gmail.com>
2022-08-27 16:40:01 +0800

    cloudinary setup

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,9 @@
 class ReportsController < ApplicationController
+
+  private
+
+  def report_params
+    params.require(:report).permit(:shop_url, :shop_name, :shop_location, :med_name,
+                                   :effects, :comments, :online, photos: [])
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,4 @@
 class Report < ApplicationRecord
   belongs_to :user
+  has_many_attached :photos
 end
