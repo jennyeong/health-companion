ec849241b5f86b8be4952682dea5ce2adaaa6fb9
Chee Jenn Yeong <cheejennyeong95@gmail.com>
2022-09-03 16:40:32 +0800

    added barcode scanner + scanning page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def scanning
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/scanning', to: "pages#scanning"
+  post '/medicine', to: "medicines#scanning"
 
   resources :medicines, only: [:show]
   # Defines the root path route ("/")
   # root "articles#index"
- 
+
 
   resources :reports, only: [:new, :create, :index, :show]
 end
