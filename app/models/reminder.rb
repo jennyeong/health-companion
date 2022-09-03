7d933b47ff1a1f8e8b65ef28b30b21d62cc5c297
Dany <98946569+danyyaw@users.noreply.github.com>
2022-09-03 09:16:31 +0800

    Update app/models/reminder.rb
    Co-authored-by: Jenn Yeong <cheejennyeong95@gmail.com>

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,6 +1,6 @@
 class Reminder < ApplicationRecord
   belongs_to :user, optional: true
-  belongs_to :medicine
+  has_one :medicine
   validates_presence_of :interval, :start_date, :end_date, :start_time, :end_time, :dosage, :med_name
 
   scope :sort_by_latest, -> { order(start_date: :asc, start_time: :asc) }
