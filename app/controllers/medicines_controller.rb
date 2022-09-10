95f58ebb3495c968eff81fa83722319f56810c30
Chee Jenn Yeong <cheejennyeong95@gmail.com>
2022-09-10 13:43:37 +0800

    adding search button for medicine

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -7,16 +7,16 @@ class MedicinesController < ApplicationController
     @barcode = barcode_params
     # binding.pry
     # puts @barcode
-    if @barcode[:symbology] == "data-matrix"
-      if @barcode[:data] == Medicine.where(serial_num: @barcode[:data])
+    # if @barcode[:symbology] == "data-matrix"
+      if Medicine.where(serial_num: @barcode[:data]).exists?
         redirect_to successful_path
-      # binding.pry
       else
         redirect_to counterfeit_path
       end
-    else
-      redirect_to information_path
-    end
+      # binding.pry
+    # else
+    #   redirect_to information_path
+    # end
   end
 
   private
@@ -24,6 +24,4 @@ class MedicinesController < ApplicationController
   def barcode_params
     params.require(:medicine).permit(:symbology, :data)
   end
-
-
 end
