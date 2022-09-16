ea6039f1effcc8ed8dc935c63782ec45d6b11c79
Ye Zhu <dellazhuye@gmail.com>
2022-09-16 21:18:42 +0800

    fix bugs after merging reminder

--- app/javascript/controllers/search_reports_controller.js
+++ app/javascript/controllers/search_reports_controller.js
@@ -5,9 +5,9 @@ export default class extends Controller {
   static targets = ["form", "input", "list"]
 
   connect() {
-    console.log(this.formTarget)
-    console.log(this.inputTarget)
-    console.log(this.listTarget)
+    // console.log(this.formTarget)
+    // console.log(this.inputTarget)
+    // console.log(this.listTarget)
   }
 
   update() {
