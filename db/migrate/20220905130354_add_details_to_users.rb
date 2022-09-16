514398c7fa9ce429a41a9f090573bf58809c222b
dany irfan <danyyaw@hotmail.com>
2022-09-16 19:42:45 +0800

    fix reminder bug

--- db/migrate/20220905130354_add_details_to_users.rb
+++ db/migrate/20220905130354_add_details_to_users.rb
@@ -2,7 +2,7 @@ class AddDetailsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :username, :string, null: false
-    add_column :users, :contact, :string, null: false
+    add_column :users, :username, :string
+    add_column :users, :contact, :string
   end
 end
